991d254bc8d9367cfcf2d83d8a0fd1af9cf69f71
Matt Norton <norton.matthew.r@gmail.com>
2020-05-15 21:39:04 -0500

    Initial commit

---
+++ AutomateExcelReporting.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Denote the path to excel workbooks:
+excel_file_1 = 'Day-Shift.xlsx'
+excel_file_2 = 'Night-Shift.xlsx'
+
+# Import data into python-
+# Create Data-Frames for each excel sheet:
+df_first_shift = pd.read_excel(excel_file_1, sheet_name='Sheet1')
+df_second_shift = pd.read_excel(excel_file_1, sheet_name='Sheet2')
+df_third_shift = pd.read_excel(excel_file_2)  # Reads first sheet by default
+
+# pip3 install xlrd (if needed)
+print(df_first_shift)
